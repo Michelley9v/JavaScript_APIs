e9f16569151845e38732de7a5a49f6953c54b6db
Michelle Hoffmann <michelley9v@yahoo.com>
2023-08-21 22:32:30 -0500

    41.45 time

--- app.js
+++ app.js
@@ -11,13 +11,18 @@ let feedbackEle = document.querySelector("#feedback");
 let searchInput = document.querySelector("#searchWord");
 let searchBtn = document.querySelector("#submitSearch");
 let gifEle = document.querySelector("#imageContainer > img");
-
+let imageContainer = document.querySelector("#imageContainer");
+let saveBtn = document.querySelector("#saveBtn")
+let savedGifsContainer = document.querySelector("#savedGifs");
 
 //event Handlers
 searchBtn.addEventListener ("click", ()  => {
     getGif(searchInput.value);
 });
 
+saveBtn.addEventListener("click", (event) => {
+  // save the current gif
+});
 //function getGif(searchTerm) {
   //  fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchInput.value}`)
     //.then((res) => res.json())
@@ -39,9 +44,12 @@ async function getGif(searchTerm) {
     console.log(body)
     //show the gif on the dom
     gifEle.src = body.data.images.original.url;
+    // display the image container
+    imageContainer.classList.remove("hidden");
    } catch (err) {
     console.error(err);
     //show the error message on the dom
     feedbackEle.textContent = err.message;
-   }
+   }// hide the image container
+   imageContainer.classList.add("hidden");
 }
